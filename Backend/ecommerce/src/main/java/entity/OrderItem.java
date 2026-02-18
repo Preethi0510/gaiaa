9e8e220ff3e4f5970d4fabe5d043b20b81f6ba69
Preethi0510 <preethikanna0505@gmail.com>
2026-02-19 00:00:11 +0530

    cgajjehr

--- Backend/ecommerce/src/main/java/entity/OrderItem.java
+++ Backend/ecommerce/src/main/java/entity/OrderItem.java
@@ -1,51 +1,8 @@
-package entity;
+package com.preethi.ecommerce.repository;
 
-import jakarta.persistence.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.preethi.ecommerce.entity.User;
 
-@Entity
-public class OrderItem {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne
-    private Order order;
-
-    @ManyToOne
-    private Product product;
-
-    private int quantity;
-    private double price;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public Order getOrder() {
-		return order;
-	}
-	public void setOrder(Order order) {
-		this.order = order;
-	}
-	public Product getProduct() {
-		return product;
-	}
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-    // getters & setters
-    
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
